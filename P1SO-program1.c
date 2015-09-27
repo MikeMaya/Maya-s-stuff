527151a9574116d579f8c31307fc187db7b2d558
Mike <miguel.maya.27@gmail.com>
2015-09-27 14:07:14 -0500

    Update P1SO-program1.c

--- P1SO-program1.c
+++ P1SO-program1.c
@@ -1,19 +1,5 @@
 #include <stdio.h>
-int main(){
-	int n;
-	scanf("%d");
-	for(int i=0;i<n;i++){
-		for(int j=0;j<n*3;j++){
-			if(j<=n/2){
-				printf(" ");
-			}
-			else if(j<=n/2+i){
-				printf("*");
-			}
-			else if(j<=){
 
-			}
-		}
-		printf("\n");
-	}
+int main(){
+	printf("Hola Mundo\n");
 }
