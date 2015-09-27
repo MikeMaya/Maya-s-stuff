91fc86c617dddcdbed001957294bb252cbf1dacb
MikeMaya <miguel.maya.27@gmail.com>
2015-09-26 22:54:03 -0500

    Create P1SO-program1.c

---
+++ P1SO-program1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+	int n;
+	scanf("%d");
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n*3;j++){
+			if(j<=n/2){
+				printf(" ");
+			}
+			else if(j<=n/2+i){
+				printf("*");
+			}
+			else if(j<=){
+
+			}
+		}
+		printf("\n");
+	}
+}
